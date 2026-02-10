5c0691c3f95f1f8d3296244cfbc0848b2913d10a
pidi3000 <pidi3000@live.com>
2026-02-10 21:34:40 +0100

    fix: correct spelling

--- config-template.py
+++ config-template.py
@@ -17,13 +17,13 @@ COMPOSE_SUBFOLDERS = ["git_subfolder"]
 """ Folder to search for docker compose stacks"""
 
 FOLDER_GIT_BASE = Path(__file__).parent.joinpath("git_base")
-""" Fodler of the cloned git repo"""
+""" Folder of the cloned git repo"""
 
 FOLDER_RUNNING_STACK = Path(__file__).parent.joinpath("stack_running")
-""" Fodler containing all files for currently running stack version"""
+""" Folder containing all files for currently running stack version"""
 
 FOLDER_GOOD_STACK = Path(__file__).parent.joinpath("stack_good")
-""" Fodler containing stack files for last known good stack version """
+""" Folder containing stack files for last known good stack version """
 
 
 ####################################################################################################
